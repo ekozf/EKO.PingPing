be3bafc1031014aae2514c3bb4c862f459589b92
ekozf <50245213+ekozf@users.noreply.github.com>
2023-05-14 23:04:57 +0200

    Models for the user sessions

---
+++ EKO.PingPing.Shared/Models/SessionsModel.cs
@@ -0,0 +1,12 @@
+﻿using EKO.PingPing.Shared.Enums;
+
+namespace EKO.PingPing.Shared.Models;
+
+public sealed class SessionsModel : ExpiringModelBase
+{
+    public DateTime LastActiveDate { get; set; }
+    public string UserAgent { get; set; } = string.Empty;
+    public string SessionId { get; set; } = string.Empty;
+
+    public override ModelTypeEnum GetModelType() => ModelTypeEnum.Sessions;
+}
---
+++ EKO.PingPing.Shared/Models/SessionsModelList.cs
@@ -0,0 +1,10 @@
+﻿using EKO.PingPing.Shared.Enums;
+
+namespace EKO.PingPing.Shared.Models;
+
+public sealed class SessionsModelList : ExpiringModelBase
+{
+    public List<SessionsModel> Sessions { get; set; } = new();
+
+    public override ModelTypeEnum GetModelType() => ModelTypeEnum.PagedSessions;
+}
