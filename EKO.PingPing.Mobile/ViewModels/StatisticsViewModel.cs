55533c34a3428d93ca0bd00e735671d4b90cfe07
ekozf <ozveremirkaan@gmail.com>
2023-12-11 22:47:16 +0100

    Speed up load times

--- EKO.PingPing.Mobile/ViewModels/StatisticsViewModel.cs
+++ EKO.PingPing.Mobile/ViewModels/StatisticsViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Input;
 using EKO.PingPing.Infrastructure.Services.Contracts;
 using EKO.PingPing.Shared.Models;
+using static EKO.PingPing.Shared.AppConsts;
 
 namespace EKO.PingPing.Mobile.ViewModels;
 
@@ -42,11 +43,15 @@ public sealed partial class StatisticsViewModel : ObservableObject
 
         var previousMonthSpent = await GetMonthlySpent(previousMonth);
         TotalPreviouslyMonthlySpentString = $"€ {previousMonthSpent:F2}";
-    
+
         var yearlySpent = await GetYearlySpent();
         TotalYearlySpentString = $"€ {yearlySpent:F2}";
     }
 
+    /// <summary>
+    /// Load every transaction that has been made.
+    /// </summary>
+    /// <returns>Total amount of money spent.</returns>
     private async Task<double> GetTotalSpent()
     {
         var allTransactions = await GetTransactions();
@@ -54,29 +59,40 @@ public sealed partial class StatisticsViewModel : ObservableObject
         return SumAndRound(allTransactions);
     }
 
+    /// <summary>
+    /// Load all the transactions for a given month.
+    /// </summary>
+    /// <param name="month">Month to load the transactions from</param>
+    /// <returns>Balance spent this month</returns>
     private async Task<double> GetMonthlySpent(int month)
     {
         var transactions = await GetTransactions();
 
         var monthlyTransactions = transactions
-            .Where(x => x.Date.Month == month && x.Date.Year == DateTime.Now.Year)
-            .ToList();
+            .Where(x => x.Date.Month == month && x.Date.Year == DateTime.Now.Year);
 
         return SumAndRound(monthlyTransactions);
     }
 
+    /// <summary>
+    /// Load all the transactions from the beginning of the school year.
+    /// </summary>
+    /// <returns>Balance spent this school year</returns>
     private async Task<double> GetYearlySpent()
     {
         var transactions = await GetTransactions();
 
         var yearlyTransactions = transactions
-            .OrderByDescending(x => x.Date)
             .Where(x => new DateTime(DateTime.Now.Year, 9, 1) < x.Date)
-            .ToList();
+            .OrderByDescending(x => x.Date);
 
         return SumAndRound(yearlyTransactions);
     }
 
+    /// <summary>
+    /// Get all the transactions.
+    /// </summary>
+    /// <returns>All loaded transactions</returns>
     private async Task<IReadOnlyList<TransactionModel>> GetTransactions()
     {
         var hasReachedEnd = false;
@@ -85,24 +101,50 @@ public sealed partial class StatisticsViewModel : ObservableObject
 
         while (!hasReachedEnd)
         {
-            var transactions = await _pingPingService.GetTransactions(currentPage);
+            // Get all transactions as tasks and wait for them all to concurrently finish
+            var results = await Task.WhenAll(GetTransactionsAll(currentPage, AMOUNT_TO_LOAD_PAGES));
 
-            if (transactions is null || transactions.Transactions.Count is 0)
-                break;
+            // Add all the transactions to the list
+            transactionsList.AddRange(results.SelectMany(x => x.Transactions));
 
-            foreach (var transaction in transactions.Transactions)
-            {
-                transactionsList.Add(transaction);
-            }
+            // If any of the results has less than 25 transactions, we have reached the end
+            if (results.Any(x => x.Transactions.Count < 25)) // 25 is the max amount of transactions per page
+                hasReachedEnd = true;
 
-            currentPage++;
-            hasReachedEnd = transactions.HasReachedEnd;
+            // Increase the current page by the amount of pages we loaded
+            currentPage += AMOUNT_TO_LOAD_PAGES;
         }
 
         return transactionsList;
     }
 
-    private static double SumAndRound(IReadOnlyList<TransactionModel> transactions)
+    /// <summary>
+    /// Get all the transactions for a given page.
+    /// </summary>
+    /// <param name="page">Transaction history page number</param>
+    /// <returns>List of tasks of transactions</returns>
+    private IReadOnlyList<Task<PagedTransactionModel>> GetTransactionsAll(int page, int amountOfPagesToLoad)
+    {
+        var currentPage = page;
+
+        var transactionsTasks = new List<Task<PagedTransactionModel>>();
+
+        while (currentPage != page + amountOfPagesToLoad)
+        {
+            transactionsTasks.Add(_pingPingService.GetTransactions(currentPage));
+
+            currentPage++;
+        }
+
+        return transactionsTasks.AsReadOnly();
+    }
+
+    /// <summary>
+    /// Sum all the negative prices and round the result to 2 decimals.
+    /// </summary>
+    /// <param name="transactions">List of transactions</param>
+    /// <returns>Summed up and rounded total value of transactions</returns>
+    private static double SumAndRound(IEnumerable<TransactionModel> transactions)
     {
         return Math.Round(transactions.Where(x => x.Price < 0).Sum(x => Math.Abs(x.Price)), 2);
     }
